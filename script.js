5c3c773878a2411077bdea6d912926189fca7235
Willian B. Santiago <will.jimmy27@hotmail.com>
2022-05-06 15:51:48 -0300

    trabalhando na validação, separamos os digitos do número e somatório do caracteres

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-function validaCPF() {
-    return false
+function validaCPF(cpf) {
+    document.getElementById('success').style.display = 'none'
+    document.getElementById('success').style.display = 'none'
+    if (cpf.length != 11) {
+        return false
+    } else {
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring (9);
+
+        var soma = 0;
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+             
+            // charAt retorna a posição da string na lista de caracteres do cpf
+        }
+        console.log(soma)
+        return true
+    }
 }
 
 function validacao() {
-    var form = document.getElementById('form').value
+    var cpf = document.getElementById('form').value
     var resultadoValidacao = validaCPF(cpf)
         if (resultadoValidacao) {
-            document.getElementById('sucess').style.display = 'block'
+            document.getElementById('success').style.display = 'block'
         } else {
             document.getElementById('error').style.display = 'block'
         }
 }
-
-//var cpf = Number(form)
