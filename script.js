01ed488a9ec8699f54b7758026c2cf5775ac2934
Willian B. Santiago <will.jimmy27@hotmail.com>
2022-05-10 00:22:54 -0300

    validacao do segundo digito do CPF

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function validaCPF(cpf) {
-    document.getElementById('success').style.display = 'none'
-    document.getElementById('success').style.display = 'none'
     if (cpf.length != 11) {
         return false
     } else {
@@ -11,9 +9,8 @@ function validaCPF(cpf) {
         for (var i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i;
 
-            // charAt retorna a posição da string na lista de caracteres do cpf
+        // charAt retorna a posição da string na lista de caracteres do cpf
         }
-        console.log(soma)
 
         var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
 
@@ -21,14 +18,31 @@ function validaCPF(cpf) {
         if (resultado != digitos.charAt(0)) {
             return false
         }
+
+        soma = 0;
+        numeros = cpf.substring(0,10);
+
+        for(var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+
+        resultado = (soma % 11) > 2 ? 0 : 11 - (soma % 11);
+
+        // Validação do segundo dígito abaixo!
+        if (resultado != digitos.charAt(1)) {
+            return false
+        }
+
         return true
 /*console.log(digitos.toString().charAt(0) + " é a primeira posição da variável soma") <--- testando soma*/ 
-
-        
-    }
+        }
 }
 
 function validacao() {
+    
+    document.getElementById('success').style.display = 'none'
+    document.getElementById('success').style.display = 'none'
+
     var cpf = document.getElementById('form').value
     var resultadoValidacao = validaCPF(cpf)
     if (resultadoValidacao) {
