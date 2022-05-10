68f749265c8c6c7795701eb0effde8ad2e3087f3
Willian B. Santiago <will.jimmy27@hotmail.com>
2022-05-10 00:09:01 -0300

    validacao do primeiro dígito do CPF

--- script.js
+++ script.js
@@ -5,25 +5,35 @@ function validaCPF(cpf) {
         return false
     } else {
         var numeros = cpf.substring(0, 9);
-        var digitos = cpf.substring (9);
+        var digitos = cpf.substring(9);
 
         var soma = 0;
         for (var i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i;
-             
+
             // charAt retorna a posição da string na lista de caracteres do cpf
         }
         console.log(soma)
+
+        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+
+        // Validação do primeiro dígito abaixo!
+        if (resultado != digitos.charAt(0)) {
+            return false
+        }
         return true
+/*console.log(digitos.toString().charAt(0) + " é a primeira posição da variável soma") <--- testando soma*/ 
+
+        
     }
 }
 
 function validacao() {
     var cpf = document.getElementById('form').value
     var resultadoValidacao = validaCPF(cpf)
-        if (resultadoValidacao) {
-            document.getElementById('success').style.display = 'block'
-        } else {
-            document.getElementById('error').style.display = 'block'
-        }
+    if (resultadoValidacao) {
+        document.getElementById('success').style.display = 'block'
+    } else {
+        document.getElementById('error').style.display = 'block'
+    }
 }
