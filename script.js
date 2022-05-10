f6b480c6a9c563c6028b153df8322aeba74559b8
Willian B. Santiago <will.jimmy27@hotmail.com>
2022-05-10 17:19:02 -0300

    corrigindo erro de operador relacional

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function validaCPF(cpf) {
         for (var i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i;
 
-        // charAt retorna a posição da string na lista de caracteres do cpf
+            // charAt retorna a posição da string na lista de caracteres do cpf
         }
 
         var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
@@ -20,13 +20,13 @@ function validaCPF(cpf) {
         }
 
         soma = 0;
-        numeros = cpf.substring(0,10);
+        numeros = cpf.substring(0, 10);
 
-        for(var k = 11; k > 1; k--) {
+        for (var k = 11; k > 1; k--) {
             soma += numeros.charAt(11 - k) * k;
         }
 
-        resultado = (soma % 11) > 2 ? 0 : 11 - (soma % 11);
+        resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
 
         // Validação do segundo dígito abaixo!
         if (resultado != digitos.charAt(1)) {
@@ -34,8 +34,8 @@ function validaCPF(cpf) {
         }
 
         return true
-/*console.log(digitos.toString().charAt(0) + " é a primeira posição da variável soma") <--- testando soma*/ 
-        }
+        /*console.log(digitos.toString().charAt(0) + " é a primeira posição da variável soma") <--- testando soma*/
+    }
 }
 
 function validacao() {
